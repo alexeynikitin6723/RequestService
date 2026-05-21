4644d10783e03fd5ca0727f69cc70a73ac2dc821
Alexey Nikitin <alexey.nikitin6723@gmail.com>
2026-05-21 21:55:45 +0400

    Удалены лишние файлы, добавлен swagger, entrypoint

--- RequestService.API/Program.cs
+++ RequestService.API/Program.cs
@@ -1,22 +1,24 @@
-var builder = WebApplication.CreateBuilder(args);
+using RequestService.Infrastructure;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddInfrastructure(builder.Configuration);
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    _ = app.UseSwagger();
+    _ = app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
