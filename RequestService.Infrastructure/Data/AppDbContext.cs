31172373cff0bd398d08097541a316a0a0dbc78a
Alexey Nikitin <alexey.nikitin6723@gmail.com>
2026-05-21 21:53:02 +0400

    Добавлен EF контекст

---
+++ RequestService.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RequestService.Domain.Models;
+
+namespace RequestService.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<DocumentRequest> Requests => Set<DocumentRequest>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            _ = modelBuilder.Entity<DocumentRequest>()
+                .HasIndex(dr => new { dr.EmployeeName, dr.DocumentType, dr.Status });
+        }
+    }
+}
---
+++ RequestService.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RequestService.Infrastructure.Data;
+
+namespace RequestService.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            _ = services.AddDbContext<AppDbContext>(options =>
+            {
+                _ = options.UseNpgsql(configuration.GetConnectionString(nameof(AppDbContext)));
+            });
+            return services;
+        }
+    }
+}
